52014f0bd480b84c9cde61e29cee603661c9065e
Joseph Moran <tjmoran@nic.edu>
2025-05-06 23:37:08 -0700

    Removed pinecone class temporarily

--- pinecone.cpp
+++ pinecone.cpp
@@ -1,30 +1,30 @@
 #include "../include/pinecone.h"
 
-Pinecone::Pinecone(sf::Vector2f startPos, Enemy* target, float speed, float damage, sf::Texture& texture)
-    : target(target), speed(speed), damage(damage)
-{
-    sprite.setTexture(texture);
-    sprite.setPosition(startPos);
+// Pinecone::Pinecone(sf::Vector2f startPos, Enemy* target, float speed, float damage, sf::Texture& texture)
+//     : target(target), speed(speed), damage(damage)
+// {
+//     sprite.setTexture(texture);
+//     sprite.setPosition(startPos);
 
-    if (target) {
-        sf::Vector2f dir = target->getPosition() - startPos;
-        float length = std::sqrt(dir.x * dir.x + dir.y * dir.y);
-        velocity = (length != 0) ? (dir / length) * speed : sf::Vector2f(0.f, 0.f);
-    }
-}
+//     if (target) {
+//         sf::Vector2f dir = target->getPosition() - startPos;
+//         float length = std::sqrt(dir.x * dir.x + dir.y * dir.y);
+//         velocity = (length != 0) ? (dir / length) * speed : sf::Vector2f(0.f, 0.f);
+//     }
+// }
 
-void Pinecone::update(float deltaTime) {
-    sprite.move(velocity * deltaTime);
-}
+// void Pinecone::update(float deltaTime) {
+//     sprite.move(velocity * deltaTime);
+// }
 
-bool Pinecone::hasHitTarget() const {
-    return target && sprite.getGlobalBounds().intersects(target->getBounds());
-}
+// bool Pinecone::hasHitTarget() const {
+//     return target && sprite.getGlobalBounds().intersects(target->getBounds());
+// }
 
-bool Pinecone::outOfRange(const sf::FloatRect& bounds) const {
-    return !bounds.intersects(sprite.getGlobalBounds());
-}
+// bool Pinecone::outOfRange(const sf::FloatRect& bounds) const {
+//     return !bounds.intersects(sprite.getGlobalBounds());
+// }
 
-sf::Sprite& Pinecone::getSprite() {
-    return sprite;
-}
+// sf::Sprite& Pinecone::getSprite() {
+//     return sprite;
+// }
